88bed450675d4acd48ae4d3979c404dfb11d130f
Yao Jean-Claude <jeandev84@yahoo.com>
2019-10-26 23:08:31 +0300

    Adding class Person

---
+++ app/Entity/Person.php
@@ -0,0 +1,117 @@
+<?php
+namespace App\Entity;
+
+
+/**
+ * Class Person
+ * @package App\Entity
+ */
+class Person
+{
+    public $name;
+    public $age;
+    public $height;
+    public $firstName;
+    public $lastName;
+
+    /**
+     * @return mixed
+     */
+    public function getFirstName()
+    {
+        return $this->firstName;
+    }
+
+    /**
+     * @param mixed $firstName
+     * @return Person
+     */
+    public function setFirstName($firstName)
+    {
+        $this->firstName = $firstName;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLastName()
+    {
+        return $this->lastName;
+    }
+
+    /**
+     * @param mixed $lastName
+     * @return Person
+     */
+    public function setLastName($lastName)
+    {
+        $this->lastName = $lastName;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     * @return Person
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAge()
+    {
+        return $this->age;
+    }
+
+    /**
+     * @param mixed $age
+     * @return Person
+     */
+    public function setAge($age)
+    {
+        $this->age = $age;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getHeight()
+    {
+        return $this->height;
+    }
+
+    /**
+     * @param mixed $height
+     * @return Person
+     */
+    public function setHeight($height)
+    {
+        $this->height = $height;
+        return $this;
+    }
+
+    public function __toString()
+    {
+        return sprintf(
+            'Person[firstName: %s, lastName: %s]',
+            $this->firstName,
+            $this->lastName
+        );
+
+    }
+
+}
